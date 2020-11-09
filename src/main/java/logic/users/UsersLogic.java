c8370425e7f72b3db8bd8a85b70f1b11eb903b99
MelaniaGP <melanya_2001@yahoo.com>
2020-11-09 19:01:30 +0100

    UsersLogic; not done yet

--- src/main/java/logic/users/UsersLogic.java
+++ src/main/java/logic/users/UsersLogic.java
@@ -5,9 +5,39 @@ import communicationWithThirdTier.TemporaryDatabase;
 import shared.User;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UsersLogic
 {
-   TemporaryDatabase db = new TemporaryDatabase();
+  public static void main(String[] args) {
+
+   TString dbURL = "jdbc:postgresql://localhost:1099/postgres?currentSchema=base";
+   String user = "postgres";
+   String password = "password";
+   
+
+   try (Connection con = DriverManager.getConnection(dbURL, user, password);
+      PreparedStatement pst = con.prepareStatement("SELECT * FROM username");
+      ResultSet rs = pst.executeQuery()) {
+
+        while (rs.next()) {
+        
+            System.out.print(rs.getInt(1));
+            System.out.print(": ");
+            System.out.println(rs.getString(2));
+        }
+
+    } catch (SQLException ex) {
+
+      System.out.println("Error occurred");
+      ex.printStackTrace();
+    }
+   }
+      
+      
   public UsersLogic()
   {
   }
